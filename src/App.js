d479f5323786b6db3413ba493dded6b09137b71d
Ugur Keskekci <ugur.keskekci@hotmail.com>
2023-02-27 01:29:28 +0300

    login page updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+//import TodoForm from "./components/TodoForm";
+import LoginForm from "./components/LoginForm";
+
 
 function App() {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  );
+  //return <TodoForm/>
+  return <LoginForm/>
 }
 
 export default App;
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {useState, useEffect} from 'react'
+
+
+const TodoForm = () => {
+    return (
+
+        <form action="" >
+            <input 
+            type='text' 
+            id='form-input' 
+            placeholder='Please enter your todo'></input>
+        </form>
+
+
+    );
+
+};
+
+export default TodoForm;
