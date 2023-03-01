f272e6c2a29596604651bcb786de1d59410b1610
Ugur Keskekci <ugur.keskekci@hotmail.com>
2023-03-01 22:07:24 +0300

    Forum page updated

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,6 +6,7 @@ function Button({onClick}) {
         <div className="mt-6">
                         <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600" onClick={onClick}>
                             Login
+                          
                         </button>
           </div>
     </>
